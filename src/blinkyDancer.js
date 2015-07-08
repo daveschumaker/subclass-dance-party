045c0e7a142b1adc0cc6dcbd09b98db814cec224
Hack Reactor Students <accounts@hackreactor.com>
2015-07-08 16:15:51 -0700

    It works!

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -10,7 +10,7 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 BlinkyDancer.prototype.step = function(){
   // call the old version of step at the beginning of any call to this new version of step
   //oldStep();
-  Dancer.prototype.step.apply(this, arguments);
+  Dancer.prototype.step.call(this, this.timeBetweenSteps);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
--- src/dancer.js
+++ src/dancer.js
@@ -1,10 +1,10 @@
 // Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps){
-  this.top = top;
-  this.left = left;
+  // this.top = top;
+  // this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
   this.$node = $('<span class="dancer"></span>');
-
+  this.setPosition(top,left);
   this.step();
 };
 
@@ -12,10 +12,10 @@ Dancer.prototype.step = function(){
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
-Dancer.prototype.setPosition = function(){
+Dancer.prototype.setPosition = function(top, left){
   var styleSettings = {
-    top: this.top,
-    left: this.left
+    top: top,
+    left: left
   };
   this.$node.css(styleSettings);
 };
