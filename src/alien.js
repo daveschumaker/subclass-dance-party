9747f49c847ab4c5830e884c2e02b5b60314d98a
Hack Reactor Students <accounts@hackreactor.com>
2015-07-08 18:57:02 -0700

    Updated our dancers to not have borders.

--- src/alien.js
+++ src/alien.js
@@ -9,8 +9,8 @@ AlienDancer.prototype.constructor = AlienDancer;
 AlienDancer.prototype.setPosition = function(top, left){
   var styleSettings = {
     top: top,
-    left: left,
-    border: '15px solid green'
+    left: left
+    //border: '15px solid green'
   };
   this.$node.css(styleSettings);
 };
